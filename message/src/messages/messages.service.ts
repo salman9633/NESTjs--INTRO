8e429dd7595879509c1418a416e63e343ae4aec3
salmanfaris <salmanvaipees913@gmail.com>
2023-03-18 13:46:04 +0530

    created repo and services

---
+++ message/src/messages/messages.service.ts
@@ -0,0 +1,18 @@
+import {MessageRepository} from './messages.repository'
+export class MessagesService{
+    messageRepo:MessageRepository
+    constructor(){
+        //service is creating it's own dependencies
+        this.messageRepo= new MessageRepository()
+    }
+
+    findOne(id:string){
+        return this.messageRepo.findOne(id)
+    }
+    findAll(){
+        return this.messageRepo.findAll()
+    }
+    create(content:string){
+        return this.messageRepo.create(content)
+    }
+}
